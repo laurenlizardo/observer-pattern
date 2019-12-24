91f5e61e617172e7aca350cf63b087e2bb05c9cb
Lauren <me@lauren-lizardo.com>
2019-12-24 13:47:30 -0800

    Added Examples folder and scripts

---
+++ Examples/TestObserver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObserver : MonoBehaviour, IObserver
+{
+  private TestObserver _instance;
+  public TestObserver Instance => _instance;
+
+  public void UpdateObserver() { }
+}
