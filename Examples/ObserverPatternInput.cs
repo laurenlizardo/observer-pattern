9b2e5b88136919dbabb832f915198511f412b339
Lauren <me@lauren-lizardo.com>
2019-12-24 15:20:05 -0800

    Created example scripts implementing ISubject and IObserver interfaces

---
+++ Examples/ObserverPatternInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ObserverPatternInput : MonoBehaviour
+{
+#region MonoBehaviour Methods
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.UpArrow)) ScoreSubject.Instance.UpdateScore(10);
+    else if (Input.GetKeyDown(KeyCode.DownArrow)) ScoreSubject.Instance.UpdateScore(-10);
+  }
+#endregion
+}
---
+++ Examples/ScoreSubject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSubject : MonoBehaviour, ISubject
+{
+  private static ScoreSubject _instance;
+  public static ScoreSubject Instance => _instance;
+
+  public int Score;
+
+#region Singleton Pattern
+  private void Awake()
+  {
+    if (_instance == null) _instance = this;
+    else Destroy(gameObject);
+  }
+#endregion
+
+#region Private Methods
+  public void UpdateScore(int points)
+  {
+    Score += points;
+    NotifyObservers();
+  }
+#endregion
+
+#region Observer Pattern
+  private List<IObserver> _observers = new List<IObserver>();
+
+  public void RegisterObserver(IObserver o)
+  {
+    if (!_observers.Contains(o)) _observers.Add(o);
+  }
+
+  public void UnregisterObserver(IObserver o)
+  {
+    if (_observers.Contains(o)) _observers.Remove(o);
+  }
+
+  public void NotifyObservers()
+  {
+    foreach (IObserver o in _observers)
+    {
+      o.UpdateObserver();
+    }
+  }
+#endregion
+}
