7ebe45a190953bf3a0fe8b60d768279fc76f990b
Lauren <me@lauren-lizardo.com>
2019-12-27 18:21:27 -0800

    Updated input

--- Examples/Scripts/ObserverPatternInput.cs
+++ Examples/Scripts/ObserverPatternInput.cs
@@ -5,8 +5,16 @@ public class ObserverPatternInput : MonoBehaviour
 #region MonoBehaviour Methods
   private void Update()
   {
-    if (Input.GetKeyDown(KeyCode.UpArrow)) ScoreSubject.Instance.UpdateScore(10);
-    else if (Input.GetKeyDown(KeyCode.DownArrow)) ScoreSubject.Instance.UpdateScore(-10);
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      if (SubjectScoreManager.Instance != null) SubjectScoreManager.Instance.UpdateScore(10);
+      if (GenericSubjectScoreManager.Instance != null) GenericSubjectScoreManager.Instance.UpdateScore(10);
+    }
+    else if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      if (SubjectScoreManager.Instance != null) SubjectScoreManager.Instance.UpdateScore(-10);
+      if (GenericSubjectScoreManager.Instance != null) GenericSubjectScoreManager.Instance.UpdateScore(-10);
+    }
   }
 #endregion
 }
