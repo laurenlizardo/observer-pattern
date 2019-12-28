cafcd148eeb52985018e06c6403fefe4afdb6dcd
Lauren <me@lauren-lizardo.com>
2019-12-27 18:15:00 -0800

    Added generic examples

---
+++ Examples/Scripts/GenericObserverCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenericObserverCanvas : MonoBehaviour, IGenericObserver<GenericSubjectScoreManager>
+{
+  public Text ScoreText;
+
+#region MonoBehaviour Methods
+  private void Start()
+  {
+    GenericSubjectScoreManager.Instance.RegisterObserver(this);
+    ScoreText.text = string.Format("Score: {0}", GenericSubjectScoreManager.Instance.Score);
+  }
+
+  private void OnDestroy()
+  {
+    GenericSubjectScoreManager.Instance.UnregisterObserver(this);
+  }
+#endregion
+
+#region Observer Pattern
+  public void UpdateObserver()
+  { 
+    ScoreText.text = string.Format("Score: {0}", GenericSubjectScoreManager.Instance.Score);
+  }
+#endregion
+}
---
+++ Examples/Scripts/GenericSubjectScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSubjectScoreManager : MonoBehaviour, IGenericSubject<GenericObserverCanvas>
+{
+  private static GenericSubjectScoreManager _instance;
+  public static GenericSubjectScoreManager Instance => _instance;
+
+  public int Score;
+
+#region Singleton Pattern
+  private void Awake()
+  {
+    if (_instance == null) _instance = this;
+    else Destroy(gameObject);
+  }
+#endregion
+
+#region Private Methods
+  public void UpdateScore(int points)
+  {
+    Score += points;
+    NotifyObservers();
+  }
+#endregion
+
+#region Observer Pattern
+  private List<GenericObserverCanvas> _observers = new List<GenericObserverCanvas>();
+
+  public void RegisterObserver(GenericObserverCanvas o)
+  {
+    if (!_observers.Contains(o)) _observers.Add(o);
+  }
+
+  public void UnregisterObserver(GenericObserverCanvas o)
+  {
+    if (_observers.Contains(o)) _observers.Remove(o);
+  }
+
+  public void NotifyObservers()
+  {
+    foreach (GenericObserverCanvas o in _observers)
+    {
+      o.UpdateObserver();
+    }
+  }
+#endregion
+}
